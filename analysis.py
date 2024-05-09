fb3942821e2732c3baa8b981cf0c309d8779c7e9
jaked <jakedaly1997@hotmail.com>
2024-05-09 21:18:19 +0100

    scatter plot of each variable pairs using seaborn. NOTE I had to change the raw data I was using as my scatter plots were not letting me distinguish each point by species of iris flower,

--- analysis.py
+++ analysis.py
@@ -6,11 +6,15 @@ import pandas as pd
 import numpy as np
 from tabulate import tabulate
 import matplotlib.pyplot as plt
+import seaborn as sns
  
+ # Iris data set
+ # https://archive.ics.uci.edu/dataset/53/iris, linked by Andrew Beatty, was using this version of the data set for the first while but encounterd errors when i was trying to plot the scatter graphs.
+ # https://github.com/mwaskom/seaborn-data/blob/master/iris.csv, one im using, raw data here was used by Ian Mcloughlinn in his lectures
 
 # download iris data and read it into a dataframe
-url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-df = pd.read_csv(url, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+url = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv' # found here https://github.com/mwaskom/seaborn-data/blob/master/iris.csv
+df = pd.read_csv(url)
 
 # use df.describe() function to get summary statistics of the variables, https://discuss.datasciencedojo.com/t/how-to-get-summary-statistics-of-a-pandas-dataframe-in-python/1137/2
 summary = df.describe()
@@ -30,3 +34,8 @@ for column in df: # loop through each column in the dataframe, https://www.geeks
     plt.xlabel(f'{column} in centimetres') # variable name on x axis
     plt.ylabel('Frequency') # label y axis
     plt.savefig(f'{column}_histogram.png') # saves histogram with corresponding colum name to .png file, https://stackoverflow.com/questions/73912257/saving-histogram-as-jpg
+
+# creating a scatter plot for each pair of variables coloured by species, https://www.geeksforgeeks.org/python-seaborn-pairplot-method/
+sns.pairplot(df, hue='species')
+plt.savefig('scatter_plots_species.png')
+plt.show()
